58606e10285925e1429f2323bd2943fe59c4750f
Liz Goff <livingearthcreations@gmail.com>
2018-04-13 13:56:47 -0500

    button works with info displayed

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
-$(document).ready();
-console.log('Sea Creatures');
+//console.log('JS is up!');
+
+let clickCount = 0;
+
+
+$(document).ready(onReady);
+//console.log('Sea Creatures');
+
+function onReady() {
+    console.log('Sea Life');
+    $('#button').on("click", clickHandler);
+    displayCount();
+
+//how to test the button --- $('#button').on("click", function(); 
+//                             console.log('was clicked');
+
+//let addOne = $()/
+
+
+function clickHandler() {
+    console.log('was clicked');
+    clickCount++;
+    console.log(clickCount);
+    displayCount();
+    }
+
+function displayCount() {
+
+    $('#totalCount').text(clickCount);
+    console.log(clickCount);
+
+}
+
+
+
+  //  let clickCount = $('toatlCount').val
+ //   console.log(displayCount).text(clickCount);
+ //   
+
+
+// "totalCount"
+//    function displayCount(clickCount);
+  //  console.log(displaycount).text(clickCount);
+
+  // ------ span lines things up like this
+  // div lines things up like this
+  // div
+  // div
+
+}
